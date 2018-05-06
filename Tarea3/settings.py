6ce1617228ae9658f2225c79b46bfc1c93c798f0
Gaspar Ricci <gasparricci.p95@gmail.com>
2018-05-06 20:35:42 -0300

    -Ajuste de configuracion de idioma y zona horaria.\n-Modificacion de permisos de manage.py

--- Tarea3/settings.py
+++ Tarea3/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
