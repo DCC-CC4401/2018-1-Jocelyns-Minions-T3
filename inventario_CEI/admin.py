dc2775a56b0b1fd885cb0693d6602e2463ba33ea
​ Asteriones​ < josefa.robert@ug.uchile.cl>
2018-05-08 10:08:33 -0300

    usrProfile

--- inventario_CEI/admin.py
+++ inventario_CEI/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Articulo,  Prestamo_articulo
 
 # Register your models here.
+admin.site.register(Articulo)
+admin.site.register( Prestamo_articulo)
--- inventario_CEI/urls.py
+++ inventario_CEI/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns=[
     #path('',views.base,name='base'),
     path('',views.artProfile,name='artProfile'),
+    path('',views.usrProfile,name='usrProfile'),
 ]
